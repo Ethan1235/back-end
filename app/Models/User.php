b8a926de6660295f9841a807f2748721792f33b4
Ethan <757819951@qq.com>
2022-12-07 22:35:16 +0800

    用户激活

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+use Illuminate\Support\Str;
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -29,9 +31,17 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'password', 'remember_token',
     ];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($user) {
+            $user->activation_token = Str::random(10);
+        });
+    }
 
     /**
      * The attributes that should be cast.
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,6 +21,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'activated' => true,
             'password' => '$2y$10$.f08CSVuQs2ILVKsQtQyReie0zBAm1oYpMvmQQjuGwI80KvhWwf/m', // password
             'remember_token' => Str::random(10),
         ];
